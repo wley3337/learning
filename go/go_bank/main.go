181e3c635a5eac5443ead8799bb04263bb5af031
Will Ley <wley3337@gmail.com>
2024-09-22 07:42:02 -0400

    Missed cleanup in main file

--- go/go_bank/main.go
+++ go/go_bank/main.go
@@ -23,25 +23,3 @@ func main() {
 		log.Fatal(err)
 	}
 }
-
-// package main
-//
-// import (
-// 	"github.com/wley3337/learning/tree/main/go/go_bank/internals/api"
-// 	st "github.com/wley3337/learning/tree/main/go/go_bank/internals/store"
-// 	"log"
-// )
-//
-// func main() {
-// 	store, err := st.NewPostgresStore()
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-//
-// 	if err := store.Init(); err != nil {
-// 		log.Fatal(err)
-// 	}
-//
-// 	server := api.NewAPIServer(":3000", store)
-// 	server.Run()
-// }
